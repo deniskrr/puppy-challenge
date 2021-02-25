c00199015d86e85b94c7b6b287854d86b54230c5
Denis Craciunescu <Denis.Craciunescu@iquestgroup.com>
2021-02-25 20:11:56 +0200

    Add PuppyRepository and display fake data on Dashboard screen

--- app/src/main/java/com/example/androiddevchallenge/PuppyApp.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyApp.kt
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import com.example.androiddevchallenge.data.AppContainer
+import com.example.androiddevchallenge.data.puppies.PuppyRepository
 import com.example.androiddevchallenge.ui.DashboardScreen
 import com.example.androiddevchallenge.ui.NavigationViewModel
 import com.example.androiddevchallenge.ui.PuppyProfileScreen
@@ -11,25 +28,33 @@ import com.example.androiddevchallenge.ui.Screen
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
-fun PuppyApp(navigationViewModel: NavigationViewModel) {
+fun PuppyApp(appContainer: AppContainer, navigationViewModel: NavigationViewModel) {
     MyTheme {
-        AppContent(navigationViewModel = navigationViewModel)
+        AppContent(
+            puppyRepository = appContainer.puppyRepository,
+            navigationViewModel = navigationViewModel
+        )
     }
 }
 
 @Composable
-fun AppContent(navigationViewModel: NavigationViewModel) {
+fun AppContent(puppyRepository: PuppyRepository, navigationViewModel: NavigationViewModel) {
     Crossfade(targetState = navigationViewModel.currentScreen) {
         Surface(color = MaterialTheme.colors.background) {
             when (navigationViewModel.currentScreen) {
                 is Screen.Dashboard -> {
-                    DashboardScreen(navigateTo = navigationViewModel::navigateTo)
+                    DashboardScreen(
+                        puppyRepository = puppyRepository,
+                        navigateTo = navigationViewModel::navigateTo
+                    )
                 }
                 is Screen.PuppyProfile -> {
-                    PuppyProfileScreen(onBack = navigationViewModel::onBack)
+                    PuppyProfileScreen(
+                        puppyRepository = puppyRepository,
+                        onBack = navigationViewModel::onBack
+                    )
                 }
             }
         }
     }
-
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/data/puppies/FakePuppyRepository.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.data.puppies
+
+import com.example.androiddevchallenge.data.Result
+import com.example.androiddevchallenge.data.model.Puppy
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.withContext
+
+class FakePuppyRepository : PuppyRepository {
+
+    private val adopted = MutableStateFlow<Set<String>>(setOf())
+
+    override suspend fun getPuppy(puppyId: String): Result<Puppy> {
+        return withContext(Dispatchers.IO) {
+            val puppy = puppies.find { it.id == puppyId }
+            if (puppy == null) {
+                Result.Error(IllegalArgumentException("Unable to find post"))
+            } else {
+                Result.Success(puppy)
+            }
+        }
+    }
+
+    override suspend fun getPuppies(): Result<List<Puppy>> {
+        return Result.Success(puppies)
+    }
+
+    override fun observeAdopted(): Flow<Set<String>> {
+        return adopted
+    }
+
+    override suspend fun adoptPuppy(puppyId: String) {
+        val adoptedSet = adopted.value.toMutableSet()
+        adoptedSet.add(puppyId)
+        adopted.value = adoptedSet
+    }
+}
